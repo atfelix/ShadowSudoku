b002d727a34cb1e4ffd81587e773f5da0bc68408
Adam Felix <adam.tyler.felix@gmail.com>
2017-07-11 11:46:43 -0400

    Initial commit

---
+++ ShadowSudoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShadowSudoku
+//
+//  Created by atfelix on 2017-07-10.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShadowSudoku/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShadowSudoku
+//
+//  Created by atfelix on 2017-07-10.
+//  Copyright © 2017 Adam Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
