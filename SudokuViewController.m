35592e7c4a3e1c4c8d55287daf22510d50b5c02d
Adam Felix <adam.tyler.felix@gmail.com>
2017-07-12 11:43:59 -0400

    Grid is now setup.
    Tags are used for distinguishing labels.

--- SudokuViewController.m
+++ SudokuViewController.m
@@ -33,16 +33,17 @@
 }
 
 -(void)setupSudokuGrid {
-
+    [self setupGridView];
     CGFloat width = (self.view.bounds.size.width - 20) / 3;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (NSInteger i = 0; i < 3; i++) {
+        for (NSInteger j = 0; j < 3; j++) {
             UIView *view = [[UIView alloc] initWithFrame:CGRectMake(j * width, i * width, width, width)];
             view.userInteractionEnabled = YES;
             view.layer.borderColor = [UIColor blackColor].CGColor;
             view.layer.borderWidth = 2.0;
             [self.gridView addSubview:view];
+            [self setupBoxAtRow:i andColumn:j inGridView:view];
         }
     }
 }
@@ -60,6 +61,23 @@
     [gridView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:10.0].active = YES;
     [gridView.widthAnchor constraintEqualToConstant:self.view.bounds.size.width - 100].active = YES;
     [gridView.heightAnchor constraintEqualToAnchor:self.gridView.widthAnchor].active = YES;
+}
+
+-(void)setupBoxAtRow:(NSInteger)row andColumn:(NSInteger)column inGridView:(UIView *)gridView {
+    CGFloat width = gridView.bounds.size.width / 3;
+
+    for (NSInteger i = 0; i < 3; i++) {
+        for (NSInteger j = 0; j < 3; j++) {
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(j * width, i * width, width, width)];
+            label.userInteractionEnabled = YES;
+            label.layer.borderColor = [UIColor redColor].CGColor;
+            label.textAlignment = NSTextAlignmentCenter;
+            label.tag = (row * 3 + i) * 9 + column * 3 + j;
+            label.text = [NSString stringWithFormat:@"%@", @((row * 3 + i) * 9 + column * 3 + j)];
+            label.layer.borderWidth = 2.0;
+            [gridView addSubview:label];
+        }
+    }
 }
 
 @end
